76a74eeba09bf0d02bc48ebb67300d7ca53f99fd
sergeicu <serge.vasylechko@childrens.harvard.edu>
2024-02-26 17:05:35 -0500

    fix typo

--- example.sh
+++ example.sh
@@ -9,10 +9,10 @@ reg_odd_even() {
     echo Volume $c
 
     # reorder slices correctly based on .json file 
-    python reorder_slices.py vol_${c}.nii.gz 
+    python reorder_slices.py data/vol_${c}.nii.gz 
 
     # run anim non rigid registration 
-    docker run --rm -v $PWD:/data sergeicu/anima /anima/animaDenseSVFBMRegistration --sp 1 --bs 3  -o /data/vol_${c}_even_to_odd.nii.gz -r /data/vol_${c}_odd_lin.nii.gz -m /data/vol_${c}_even_lin.nii.gz #> /data/log_anima_${c}_e_o.log 2>&1  # register 
+    docker run --rm -v $PWD/data/:/data sergeicu/anima /anima/animaDenseSVFBMRegistration --sp 1 --bs 3  -o /data/vol_${c}_even_to_odd.nii.gz -r /data/vol_${c}_odd_lin.nii.gz -m /data/vol_${c}_even_lin.nii.gz #> /data/log_anima_${c}_e_o.log 2>&1  # register 
 
     # average 
     python add.py data/vol_${c}_odd_lin.nii.gz data/vol_${c}_even_to_odd.nii.gz -o data/vol_${c}_lin_odd_even_merged.nii.gz 
