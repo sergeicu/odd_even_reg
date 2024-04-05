47312f486e4189ad7131ce4d4fd560339ec2ee29
sergeicu <serge.vasylechko@childrens.harvard.edu>
2024-04-05 16:20:31 -0400

    Update install.sh - crkit docker dependencies

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ pip install -r requirements.txt
 
 # pull anima docker 
 docker pull sergeicu/anima
+
+
+# To perform isotropic resampling (as shown in example.sh) 
+# Make sure that crkit docker image is installed. It is currently installed on `ganymede` machine. 
+# To pull crkit docker to any other machine - you can find it here - 
+docker pull ccts3.aws.chboston.org:5151/computationalradiology/crkit
+# if you do not have access to BCH internal `ccts3` website - make sure you ask someone who has 
